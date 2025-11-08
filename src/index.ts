3ca73593fee9ac833d4ef71dd135bacef7ef471b
Nishant Aanjaney Jalan <cybercoder.nishant@gmail.com>
2025-11-08 21:33:00 +0000

    changing the cron schedule

--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,7 @@ export default {
 	async fetch(req) {
 		const url = new URL(req.url);
 		url.pathname = '/__scheduled';
-		url.searchParams.append('cron', '* * * * *');
+		url.searchParams.append('cron', '0 9 * * *');
 		return new Response(`To test the scheduled handler, ensure you have used the "--test-scheduled" then try running "curl ${url.href}".`);
 	},
 
